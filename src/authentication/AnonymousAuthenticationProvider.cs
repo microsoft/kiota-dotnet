5c39e2c227deccc3cb553ce7762d7c7dce388415
Vincent Biret <vibiret@microsoft.com>
2022-05-17 09:33:20 -0400

    - adds an additional parameter to authentication methods to carry contextual information
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/authentication/AnonymousAuthenticationProvider.cs
+++ src/authentication/AnonymousAuthenticationProvider.cs
@@ -2,6 +2,7 @@
 //  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 // ------------------------------------------------------------------------------
 
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,13 +13,8 @@ namespace Microsoft.Kiota.Abstractions.Authentication
     /// </summary>
     public class AnonymousAuthenticationProvider : IAuthenticationProvider
     {
-        /// <summary>
-        /// Authenticates the <see cref="RequestInformation"/> instance
-        /// </summary>
-        /// <param name="request">The request to authenticate</param>
-        /// <param name="cancellationToken">The cancellation token for the task</param>
-        /// <returns></returns>
-        public Task AuthenticateRequestAsync(RequestInformation request, CancellationToken cancellationToken = default)
+        /// <inheritdoc />
+        public Task AuthenticateRequestAsync(RequestInformation request, Dictionary<string, object> additionalAuthenticationContext = default, CancellationToken cancellationToken = default)
         {
             return Task.CompletedTask;
         }
--- src/authentication/BaseBearerTokenAuthenticationProvider.cs
+++ src/authentication/BaseBearerTokenAuthenticationProvider.cs
@@ -3,6 +3,7 @@
 // ------------------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -26,18 +27,13 @@ public class BaseBearerTokenAuthenticationProvider : IAuthenticationProvider
     public IAccessTokenProvider AccessTokenProvider {get; private set;}
     private const string AuthorizationHeaderKey = "Authorization";
 
-    /// <summary>
-    /// Authenticates the <see cref="RequestInformation"/> instance
-    /// </summary>
-    /// <param name="request">The request to authenticate</param>
-    /// <param name="cancellationToken">The cancellation token for the task</param>
-    /// <returns></returns>
-    public async Task AuthenticateRequestAsync(RequestInformation request, CancellationToken cancellationToken = default)
+    /// <inheritdoc />
+    public async Task AuthenticateRequestAsync(RequestInformation request, Dictionary<string, object> additionalAuthenticationContext = default, CancellationToken cancellationToken = default)
     {
         if(request == null) throw new ArgumentNullException(nameof(request));
         if(!request.Headers.ContainsKey(AuthorizationHeaderKey))
         {
-            var token = await AccessTokenProvider.GetAuthorizationTokenAsync(request.URI, cancellationToken);
+            var token = await AccessTokenProvider.GetAuthorizationTokenAsync(request.URI, additionalAuthenticationContext, cancellationToken);
             if(!string.IsNullOrEmpty(token))
                 request.Headers.Add(AuthorizationHeaderKey, $"Bearer {token}");
         }
