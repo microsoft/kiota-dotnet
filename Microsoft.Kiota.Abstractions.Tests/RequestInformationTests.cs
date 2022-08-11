720697903a9cc93f1d948245754e695fe0691325
Andrew Omondi <andrew.omondi@microsoft.com>
2022-08-11 11:28:08 +0300

    Fixes in the URL building dateTimes

--- Microsoft.Kiota.Abstractions.Tests/RequestInformationTests.cs
+++ Microsoft.Kiota.Abstractions.Tests/RequestInformationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Moq;
 using Xunit;
@@ -25,8 +26,6 @@ namespace Microsoft.Kiota.Abstractions.Tests
             Assert.Equal("foo",testRequest.QueryParameters.First().Key);
             Assert.Equal("bar", testRequest.QueryParameters.First().Value.ToString());
         }
-
-
         [Fact]
         public void AddsAndRemovesRequestOptions()
         {
@@ -68,6 +67,84 @@ namespace Microsoft.Kiota.Abstractions.Tests
             Assert.True(requestInfo.QueryParameters.ContainsKey("%24select"));
             Assert.False(requestInfo.QueryParameters.ContainsKey("select"));
             Assert.Equal("%24select",requestInfo.QueryParameters.First().Key);
+        }
+        [Fact]
+        public void SetsPathParametersOfDateTimeOffsetType()
+        {
+            // Arrange as the request builders would
+            var requestInfo = new RequestInformation
+            {
+                HttpMethod = Method.GET,
+                UrlTemplate = "http://localhost/getDirectRoutingCalls(fromDateTime='{fromDateTime}',toDateTime='{toDateTime}')"
+            };
+
+            // Act
+            var fromDateTime = new DateTimeOffset(2022, 8, 1, 0, 0, 0, TimeSpan.Zero);
+            var toDateTime = new DateTimeOffset(2022, 8, 2, 0, 0, 0, TimeSpan.Zero);
+            var pathParameters = new Dictionary<string, object>();
+            pathParameters.Add("fromDateTime", fromDateTime);
+            pathParameters.Add("toDateTime", toDateTime);
+
+            requestInfo.PathParameters = pathParameters;
+
+            // Assert
+            Assert.Contains("fromDateTime='2022-08-01T00%3A00%3A00.0000000%2B00%3A00'", requestInfo.URI.OriginalString);
+            Assert.Contains("toDateTime='2022-08-02T00%3A00%3A00.0000000%2B00%3A00'", requestInfo.URI.OriginalString);
+        }
+        [Fact]
+        public void SetsPathParametersOfBooleanType()
+        {
+            // Arrange as the request builders would
+            var requestInfo = new RequestInformation
+            {
+                HttpMethod = Method.GET,
+                UrlTemplate = "http://localhost/users{?%24count}"
+            };
+
+            // Act
+            var count = true;
+            var pathParameters = new Dictionary<string, object>();
+            pathParameters.Add("%24count", count);
+
+            requestInfo.PathParameters = pathParameters;
+
+            // Assert
+            Assert.Contains("%24count=true", requestInfo.URI.OriginalString);
+        }
+
+        [Fact]
+        public void ThrowsInvalidOperationExceptionWhenBaseUrlNotSet()
+        {
+            // Arrange as the request builders would
+            var requestInfo = new RequestInformation
+            {
+                HttpMethod = Method.GET,
+                UrlTemplate = "{+baseurl}/users{?%24count}"
+            };
+
+            // Assert
+            var exception = Assert.Throws<InvalidOperationException>(() => requestInfo.URI);
+            Assert.Contains("baseurl", exception.Message);
+        }
+
+        [Fact]
+        public void BuildsUrlOnProvidedBaseUrl()
+        {
+            // Arrange as the request builders would
+            var requestInfo = new RequestInformation
+            {
+                HttpMethod = Method.GET,
+                UrlTemplate = "{+baseurl}/users{?%24count}"
+            };
+
+            // Act
+            requestInfo.PathParameters = new Dictionary<string, object>() 
+            {
+                { "baseurl","http://localhost" }
+            };
+
+            // Assert
+            Assert.Contains("http://localhost", requestInfo.URI.OriginalString);
         }
     }
 
