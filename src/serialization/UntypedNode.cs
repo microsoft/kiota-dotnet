0a8026c33acb5c20c526ff8236898e90e01d7d39
Martin Machacek <machacek@edhouse.cz>
2024-01-17 13:25:29 +0100

    Create types for untyped nodes

--- src/serialization/UntypedNode.cs
+++ src/serialization/UntypedNode.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Microsoft.Kiota.Abstractions.Serialization
 {
@@ -12,6 +13,8 @@ namespace Microsoft.Kiota.Abstractions.Serialization
     /// </summary>
     public class UntypedNode : IParsable
     {
+        private static readonly IDictionary<string, Action<IParseNode>> _fieldDeserializers = new ReadOnlyDictionary<string, Action<IParseNode>>(new Dictionary<string, Action<IParseNode>>());
+        private static readonly UntypedNode _modelObject = new ();
         /// <summary>
         /// The value assigned to untyped node.
         /// </summary>
@@ -19,17 +22,20 @@ namespace Microsoft.Kiota.Abstractions.Serialization
         /// <summary>
         /// The deserialization information for the current model
         /// </summary>
-        public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers() => new Dictionary<string, Action<IParseNode>>();
+        public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers() => _fieldDeserializers;
         /// <summary>
         /// Serializes information the current object
         /// <param name="writer">Serialization writer to use to serialize this model</param>
         /// </summary>
         public virtual void Serialize(ISerializationWriter writer) => _ = writer ?? throw new ArgumentNullException(nameof(writer));
         /// <summary>
-        /// 
+        /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <param name="parseNode"></param>
-        /// <returns></returns>
-        public static UntypedNode CreateFromDiscriminator(IParseNode parseNode) => new UntypedNode();
+        /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
+        public static UntypedNode CreateFromDiscriminator(IParseNode parseNode)
+        {
+            _ = parseNode ?? throw new ArgumentNullException(nameof(parseNode));
+            return _modelObject;
+        }
     }
 }
