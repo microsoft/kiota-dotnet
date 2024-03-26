8244d4a51151e0cf81ff06b2683ed95952e2b55c
Ricardo Boss <mail@ricardoboss.de>
2024-03-26 01:09:07 +0100

    Added tests

---
+++ Microsoft.Kiota.Abstractions.Tests/MultipartBodyTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Kiota.Abstractions.Serialization;
+using Moq;
+using Xunit;
+
+namespace Microsoft.Kiota.Abstractions.Tests;
+
+public class MultipartBodyTests
+{
+    [Fact]
+    public void KeepsFilename()
+    {
+        var writerMock = new Mock<ISerializationWriter>();
+        var requestAdapterMock = new Mock<IRequestAdapter>();
+        var serializationFactoryMock = new Mock<ISerializationWriterFactory>();
+
+        var body = new MultipartBody();
+
+        requestAdapterMock
+            .Setup(r => r.SerializationWriterFactory)
+            .Returns(serializationFactoryMock.Object);
+
+        body.RequestAdapter = requestAdapterMock.Object;
+
+        writerMock.Setup(w => w.WriteStringValue("", "--" + body.Boundary));
+        writerMock.Setup(w => w.WriteStringValue("Content-Type", "application/json"));
+        writerMock.Setup(w => w.WriteStringValue("Content-Disposition", "form-data; name=\"file\"; filename=\"file.json\""));
+        writerMock.Setup(w => w.WriteStringValue("", "fileContent"));
+        writerMock.Setup(w => w.WriteStringValue("", ""));
+        writerMock.Setup(w => w.WriteStringValue("", "--" + body.Boundary + "--"));
+
+        body.AddOrReplacePart("file", "application/json", "fileContent", "file.json");
+        body.Serialize(writerMock.Object);
+
+        writerMock.VerifyAll();
+        requestAdapterMock.VerifyAll();
+        serializationFactoryMock.VerifyAll();
+    }
+
+    [Fact]
+    public void WorksWithoutFilename()
+    {
+        var writerMock = new Mock<ISerializationWriter>();
+        var requestAdapterMock = new Mock<IRequestAdapter>();
+        var serializationFactoryMock = new Mock<ISerializationWriterFactory>();
+
+        var body = new MultipartBody();
+
+        requestAdapterMock
+            .Setup(r => r.SerializationWriterFactory)
+            .Returns(serializationFactoryMock.Object);
+
+        body.RequestAdapter = requestAdapterMock.Object;
+
+        writerMock.Setup(w => w.WriteStringValue("", "--" + body.Boundary));
+        writerMock.Setup(w => w.WriteStringValue("Content-Type", "application/json"));
+        writerMock.Setup(w => w.WriteStringValue("Content-Disposition", "form-data; name=\"file\""));
+        writerMock.Setup(w => w.WriteStringValue("", "fileContent"));
+        writerMock.Setup(w => w.WriteStringValue("", ""));
+        writerMock.Setup(w => w.WriteStringValue("", "--" + body.Boundary + "--"));
+
+        body.AddOrReplacePart("file", "application/json", "fileContent");
+        body.Serialize(writerMock.Object);
+
+        writerMock.VerifyAll();
+        requestAdapterMock.VerifyAll();
+        serializationFactoryMock.VerifyAll();
+    }
+}
