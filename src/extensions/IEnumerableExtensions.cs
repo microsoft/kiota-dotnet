9b52d85e453031dfdffdf953091c4aa9a70a3031
kasperk81 <83082615+kasperk81@users.noreply.github.com>
2024-06-12 13:47:38 +0300

    Update IEnumerableExtensions.cs

--- src/extensions/IEnumerableExtensions.cs
+++ src/extensions/IEnumerableExtensions.cs
@@ -13,28 +13,44 @@ namespace Microsoft.Kiota.Abstractions.Extensions
     /// </summary>
     public static class IEnumerableExtensions
     {
+        /// <summary>
+        /// Converts an <see cref="IEnumerable{T}"/> to a <see cref="List{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the collection.</typeparam>
+        /// <param name="e">The enumerable to convert.</param>
+        /// <returns>A <see cref="List{T}"/> containing the elements of the enumerable.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the input enumerable is null.</exception>
         public static List<T> AsList<T>(this IEnumerable<T> e)
         {
-            if (e is null) ThrowHelper.ThrowArgumentNullException(nameof(e));
+            if (e is null) throw new ArgumentNullException(nameof(e), "Input collection cannot be null.");
             
             if (e is List<T> list) return list;
 
             return new List<T>(e);
         }
 
+        /// <summary>
+        /// Converts an <see cref="IEnumerable{T}"/> to an array.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the collection.</typeparam>
+        /// <param name="e">The enumerable to convert.</param>
+        /// <returns>An array containing the elements of the enumerable.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the input enumerable is null.</exception>
         public static T[] AsArray<T>(this IEnumerable<T> e)
         {
-            if (e is null) ThrowHelper.ThrowArgumentNullException(nameof(e));
+            if (e is null) throw new ArgumentNullException(nameof(e), "Input collection cannot be null.");
 
             if (e is T[] array) return array;
+
+            T[]? result = null;
 
             if (e is ICollection<T> collection)
             {
                 // Allocate an array with the exact size
 #if NET5_0_OR_GREATER
-                T[] result = GC.AllocateUninitializedArray<T>(collection.Count);
+                result = GC.AllocateUninitializedArray<T>(collection.Count);
 #else
-                T[] result = new T[count];
+                result = new T[collection.Count];
 #endif
                 collection.CopyTo(result, 0);
                 return result;
@@ -46,15 +62,15 @@ namespace Microsoft.Kiota.Abstractions.Extensions
 
             // Allocate array with the counted size
 #if NET5_0_OR_GREATER
-            T[] result = GC.AllocateUninitializedArray<T>(count);
+            result = GC.AllocateUninitializedArray<T>(count);
 #else
-            T[] result = new T[count];
+            result = new T[count];
 #endif
 
             // Second pass to copy the elements
             count = 0;
             foreach (var item in e) result[count++] = item;
-            return default(TValue);
+            return result;
         }
     }
 }
