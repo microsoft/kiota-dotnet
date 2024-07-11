eb5a36011a149c7d82435b5925565283be65a013
Martin Machacek <machacek@edhouse.cz>
2024-07-11 14:06:22 +0200

    SendPrimitiveAsync throws InvalidOperationException for enums

---
+++ tests/trimming/Microsoft.Kiota.Trimming.Validation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.Kiota.Trimming.Validation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
