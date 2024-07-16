d1b5780819d181864fc1a925211c212e63da2599
Andrew Omondi <anomondi@microsoft.com>
2024-07-16 15:14:31 +0300

    [WIP] Adds bundle package.

---
+++ src/bundle/KiotaRequestAdapter.cs
@@ -0,0 +1,47 @@
+﻿// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+using System.Net.Http;
+using Microsoft.Kiota.Abstractions;
+using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Abstractions.Serialization;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+using Microsoft.Kiota.Serialization.Form;
+using Microsoft.Kiota.Serialization.Json;
+using Microsoft.Kiota.Serialization.Multipart;
+using Microsoft.Kiota.Serialization.Text;
+
+namespace Microsoft.Kiota
+{
+    /// <summary>
+    /// The <see cref="IRequestAdapter"/> implementation that derived from <see cref="HttpClientRequestAdapter"/> with registrations configured.
+    /// </summary>
+    public class KiotaRequestAdapter : HttpClientRequestAdapter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="authenticationProvider"></param>
+        /// <param name="parseNodeFactory"></param>
+        /// <param name="serializationWriterFactory"></param>
+        /// <param name="httpClient"></param>
+        /// <param name="observabilityOptions"></param>
+        public KiotaRequestAdapter(IAuthenticationProvider authenticationProvider, IParseNodeFactory? parseNodeFactory = null, ISerializationWriterFactory? serializationWriterFactory = null, HttpClient? httpClient = null, ObservabilityOptions? observabilityOptions = null) : base(authenticationProvider, parseNodeFactory, serializationWriterFactory, httpClient, observabilityOptions)
+        {
+            SetupDefaults();
+        }
+
+        private static void SetupDefaults() 
+        {
+            // Setup the default serializers/deserializers
+            ApiClientBuilder.RegisterDefaultSerializer<JsonSerializationWriterFactory>();
+            ApiClientBuilder.RegisterDefaultSerializer<TextSerializationWriterFactory>();
+            ApiClientBuilder.RegisterDefaultSerializer<FormSerializationWriterFactory>();
+            ApiClientBuilder.RegisterDefaultSerializer<MultipartSerializationWriterFactory>();
+            ApiClientBuilder.RegisterDefaultDeserializer<JsonParseNodeFactory>();
+            ApiClientBuilder.RegisterDefaultDeserializer<TextParseNodeFactory>();
+            ApiClientBuilder.RegisterDefaultDeserializer<FormParseNodeFactory>();
+        }
+    }
+}
---
+++ tests/bundle/BundleTests.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+using Moq;
+using System;
+using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Abstractions.Serialization;
+
+namespace Microsoft.Kiota.Bundle.Tests
+{
+    public class BundleTests
+    {
+        [Fact]
+        public void ThrowsArgumentNullExceptionOnNullAuthenticationProvider()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => new KiotaRequestAdapter(null!));
+            Assert.Equal("authenticationProvider", exception.ParamName);
+        }
+
+        [Fact]
+        public void SerializersAreRegisteredAsExpected()
+        {
+            // setup
+            KiotaRequestAdapter requestAdapter = new KiotaRequestAdapter(new AnonymousAuthenticationProvider());
+
+            // validate
+            var serializerCount = SerializationWriterFactoryRegistry.DefaultInstance.ContentTypeAssociatedFactories.Count;
+            var deserializerCount = ParseNodeFactoryRegistry.DefaultInstance.ContentTypeAssociatedFactories.Count;
+
+            Assert.Equal(4, serializerCount); // four serializers present
+            Assert.Equal(3, deserializerCount);// three deserializers present
+
+            var serializerKeys = SerializationWriterFactoryRegistry.DefaultInstance.ContentTypeAssociatedFactories.Keys;
+            var deserializerKeys = ParseNodeFactoryRegistry.DefaultInstance.ContentTypeAssociatedFactories.Keys;
+
+            Assert.Contains("application/json", serializerKeys);
+            Assert.Contains("application/json", deserializerKeys);// Serializer and deserializer present for application/json
+
+            Assert.Contains("text/plain", serializerKeys);
+            Assert.Contains("text/plain", deserializerKeys);// Serializer and deserializer present for text/plain
+
+            Assert.Contains("application/x-www-form-urlencoded", serializerKeys);
+            Assert.Contains("application/x-www-form-urlencoded", deserializerKeys);// Serializer and deserializer present for application/x-www-form-urlencoded
+
+            Assert.Contains("multipart/form-data", serializerKeys);// Serializer present for multipart/form-data
+        }
+    }
+}
