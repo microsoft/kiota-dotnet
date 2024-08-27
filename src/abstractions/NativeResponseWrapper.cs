c7df8014595a01a22a9bab615021a975d6e1b59c
Vincent Biret <vibiret@microsoft.com>
2024-08-27 14:07:34 -0400

    fix: adds missing cancellation token parameter
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- src/abstractions/NativeResponseWrapper.cs
+++ src/abstractions/NativeResponseWrapper.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.Kiota.Abstractions
@@ -30,7 +31,7 @@ namespace Microsoft.Kiota.Abstractions
                 Func<Action<QueryParametersType>?, Action<IDictionary<string, string>>?, IEnumerable<IRequestOption>?, IResponseHandler, Task<ModelType>> originalCall,
                 Action<QueryParametersType>? q = default,
                 Action<IDictionary<string, string>>? h = default,
-                IEnumerable<IRequestOption>? o = default) where NativeResponseType : class => CallAndGetNativeTypeAsync<ModelType, NativeResponseType, QueryParametersType>(originalCall, q, h, o);
+                IEnumerable<IRequestOption>? o = default) where NativeResponseType : class => CallAndGetNativeTypeAsync<ModelType, NativeResponseType, QueryParametersType>((_q, _h, _o, _r, _c) => originalCall(_q, _h, _o, _r), q, h, o);
 
         /// <summary>
         /// Makes a request with the <typeparam name="RequestBodyType"/> and <typeparam name="QueryParametersType"/> instances to get a response with
@@ -48,7 +49,7 @@ namespace Microsoft.Kiota.Abstractions
                 RequestBodyType requestBody,
                 Action<QueryParametersType>? q = default,
                 Action<IDictionary<string, string>>? h = default,
-                IEnumerable<IRequestOption>? o = default) where NativeResponseType : class => CallAndGetNativeTypeAsync<ModelType, NativeResponseType, QueryParametersType, RequestBodyType>(originalCall, requestBody, q, h, o);
+                IEnumerable<IRequestOption>? o = default) where NativeResponseType : class => CallAndGetNativeTypeAsync<ModelType, NativeResponseType, QueryParametersType, RequestBodyType>((_b, _q, _h, _o, _r, _c) => originalCall(_b, _q, _h, _o, _r), requestBody, q, h, o);
 #pragma warning restore VSTHRD200 // Use "Async" suffix for async methods
         /// <summary>
         /// Makes a request with the <typeparam name="QueryParametersType"/> instance to get a response with
@@ -58,15 +59,17 @@ namespace Microsoft.Kiota.Abstractions
         /// <param name="q">The query parameters of the request</param>
         /// <param name="h">The request headers of the request</param>
         /// <param name="o">Request options</param>
+        /// <param name="cancellationToken">The cancellation token</param>
         /// <returns></returns>
         public static async Task<NativeResponseType?> CallAndGetNativeTypeAsync<ModelType, NativeResponseType, QueryParametersType>(
-                Func<Action<QueryParametersType>?, Action<IDictionary<string, string>>?, IEnumerable<IRequestOption>?, IResponseHandler, Task<ModelType>> originalCall,
+                Func<Action<QueryParametersType>?, Action<IDictionary<string, string>>?, IEnumerable<IRequestOption>?, IResponseHandler, CancellationToken, Task<ModelType>> originalCall,
                 Action<QueryParametersType>? q = default,
                 Action<IDictionary<string, string>>? h = default,
-                IEnumerable<IRequestOption>? o = default) where NativeResponseType : class
+                IEnumerable<IRequestOption>? o = default,
+                CancellationToken cancellationToken = default) where NativeResponseType : class
         {
             var responseHandler = new NativeResponseHandler();
-            await originalCall.Invoke(q, h, o, responseHandler);
+            await originalCall.Invoke(q, h, o, responseHandler, cancellationToken);
             return responseHandler.Value as NativeResponseType;
         }
 
@@ -79,15 +82,17 @@ namespace Microsoft.Kiota.Abstractions
         /// <param name="q">The query parameters of the request</param>
         /// <param name="h">The request headers of the request</param>
         /// <param name="o">Request options</param>
+        /// <param name="cancellationToken">The cancellation token</param>
         public static async Task<NativeResponseType?> CallAndGetNativeTypeAsync<ModelType, NativeResponseType, QueryParametersType, RequestBodyType>(
-                Func<RequestBodyType, Action<QueryParametersType>?, Action<IDictionary<string, string>>?, IEnumerable<IRequestOption>?, IResponseHandler, Task<ModelType>> originalCall,
+                Func<RequestBodyType, Action<QueryParametersType>?, Action<IDictionary<string, string>>?, IEnumerable<IRequestOption>?, IResponseHandler, CancellationToken, Task<ModelType>> originalCall,
                 RequestBodyType requestBody,
                 Action<QueryParametersType>? q = default,
                 Action<IDictionary<string, string>>? h = default,
-                IEnumerable<IRequestOption>? o = default) where NativeResponseType : class
+                IEnumerable<IRequestOption>? o = default,
+                CancellationToken cancellationToken = default) where NativeResponseType : class
         {
             var responseHandler = new NativeResponseHandler();
-            await originalCall.Invoke(requestBody, q, h, o, responseHandler);
+            await originalCall.Invoke(requestBody, q, h, o, responseHandler, cancellationToken);
             return responseHandler.Value as NativeResponseType;
         }
     }
