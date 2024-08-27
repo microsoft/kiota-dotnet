ebf15e414ec3a9254269fcfbd1e92f3fbac17b7e
Vincent Biret <vibiret@microsoft.com>
2024-08-27 13:30:00 -0400

    chore: linting of native wrapper

--- src/abstractions/NativeResponseWrapper.cs
+++ src/abstractions/NativeResponseWrapper.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Threading.Tasks;
 
 namespace Microsoft.Kiota.Abstractions
@@ -22,7 +23,43 @@ namespace Microsoft.Kiota.Abstractions
         /// <param name="h">The request headers of the request</param>
         /// <param name="o">Request options</param>
         /// <returns></returns>
-        public static async Task<NativeResponseType?> CallAndGetNativeType<ModelType, NativeResponseType, QueryParametersType>(
+        [Obsolete("Use CallAndGetNativeTypeAsync instead")]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+#pragma warning disable VSTHRD200 // Use "Async" suffix for async methods
+        public static Task<NativeResponseType?> CallAndGetNativeType<ModelType, NativeResponseType, QueryParametersType>(
+                Func<Action<QueryParametersType>?, Action<IDictionary<string, string>>?, IEnumerable<IRequestOption>?, IResponseHandler, Task<ModelType>> originalCall,
+                Action<QueryParametersType>? q = default,
+                Action<IDictionary<string, string>>? h = default,
+                IEnumerable<IRequestOption>? o = default) where NativeResponseType : class => CallAndGetNativeTypeAsync<ModelType, NativeResponseType, QueryParametersType>(originalCall, q, h, o);
+
+        /// <summary>
+        /// Makes a request with the <typeparam name="RequestBodyType"/> and <typeparam name="QueryParametersType"/> instances to get a response with
+        /// a <typeparam name="NativeResponseType"/> instance and expect an instance of <typeparam name="ModelType"/>
+        /// </summary>
+        /// <param name="originalCall">The original request to make</param>
+        /// <param name="requestBody">The request body of the request</param>
+        /// <param name="q">The query parameters of the request</param>
+        /// <param name="h">The request headers of the request</param>
+        /// <param name="o">Request options</param>
+        [Obsolete("Use CallAndGetNativeTypeAsync instead")]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public static Task<NativeResponseType?> CallAndGetNativeType<ModelType, NativeResponseType, QueryParametersType, RequestBodyType>(
+                Func<RequestBodyType, Action<QueryParametersType>?, Action<IDictionary<string, string>>?, IEnumerable<IRequestOption>?, IResponseHandler, Task<ModelType>> originalCall,
+                RequestBodyType requestBody,
+                Action<QueryParametersType>? q = default,
+                Action<IDictionary<string, string>>? h = default,
+                IEnumerable<IRequestOption>? o = default) where NativeResponseType : class => CallAndGetNativeTypeAsync<ModelType, NativeResponseType, QueryParametersType, RequestBodyType>(originalCall, requestBody, q, h, o);
+#pragma warning restore VSTHRD200 // Use "Async" suffix for async methods
+        /// <summary>
+        /// Makes a request with the <typeparam name="QueryParametersType"/> instance to get a response with
+        /// a <typeparam name="NativeResponseType"/> instance and expect an instance of <typeparam name="ModelType"/>
+        /// </summary>
+        /// <param name="originalCall">The original request to make</param>
+        /// <param name="q">The query parameters of the request</param>
+        /// <param name="h">The request headers of the request</param>
+        /// <param name="o">Request options</param>
+        /// <returns></returns>
+        public static async Task<NativeResponseType?> CallAndGetNativeTypeAsync<ModelType, NativeResponseType, QueryParametersType>(
                 Func<Action<QueryParametersType>?, Action<IDictionary<string, string>>?, IEnumerable<IRequestOption>?, IResponseHandler, Task<ModelType>> originalCall,
                 Action<QueryParametersType>? q = default,
                 Action<IDictionary<string, string>>? h = default,
@@ -42,7 +79,7 @@ namespace Microsoft.Kiota.Abstractions
         /// <param name="q">The query parameters of the request</param>
         /// <param name="h">The request headers of the request</param>
         /// <param name="o">Request options</param>
-        public static async Task<NativeResponseType?> CallAndGetNativeType<ModelType, NativeResponseType, QueryParametersType, RequestBodyType>(
+        public static async Task<NativeResponseType?> CallAndGetNativeTypeAsync<ModelType, NativeResponseType, QueryParametersType, RequestBodyType>(
                 Func<RequestBodyType, Action<QueryParametersType>?, Action<IDictionary<string, string>>?, IEnumerable<IRequestOption>?, IResponseHandler, Task<ModelType>> originalCall,
                 RequestBodyType requestBody,
                 Action<QueryParametersType>? q = default,
